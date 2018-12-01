ccc76a9fb1be6edb0d3c068dd7c9319a0140e416
Valentin Stets <valik.stets@gmail.com>
2018-12-01 19:36:35 +0200

    Navigation added

--- src/views/Dashboard/Dashboard.jsx
+++ src/views/Dashboard/Dashboard.jsx
@@ -13,6 +13,7 @@ import CardBody from "components/Card/CardBody.jsx";
 import CardFooter from "components/Card/CardFooter.jsx";
 
 import dashboardStyle from "assets/jss/material-dashboard-react/views/dashboardStyle.jsx";
+import { Link } from "react-router-dom";
 
 class Dashboard extends React.Component {
   state = {
@@ -31,34 +32,38 @@ class Dashboard extends React.Component {
       <div>
         <GridContainer>
           <GridItem xs={12} sm={12} md={4}>
-            <Card chart>
-              <CardHeader color="success">
-                <div className={classes.cardTitleWhite}>
-                  <h3>2D</h3>
-                </div>
-              </CardHeader>
-              <CardBody>
-                <h4 className={classes.cardTitle}>Симуляція у двовимірному просторі</h4>
-              </CardBody>
-              <CardFooter chart>
-                <div className={classes.stats}>12 симуляцій</div>
-              </CardFooter>
-            </Card>
+            <Link to="2dviz">
+              <Card chart>
+                <CardHeader color="success">
+                  <div className={classes.cardTitleWhite}>
+                    <h3>2D</h3>
+                  </div>
+                </CardHeader>
+                <CardBody>
+                  <h4 className={classes.cardTitle}>Симуляція у двовимірному просторі</h4>
+                </CardBody>
+                <CardFooter chart>
+                  <div className={classes.stats}>12 симуляцій</div>
+                </CardFooter>
+              </Card>
+            </Link>
           </GridItem>
           <GridItem xs={12} sm={12} md={4}>
-            <Card chart>
-              <CardHeader color="warning">
-                <div className={classes.cardTitleWhite}>
-                  <h3>3D</h3>
-                </div>
-              </CardHeader>
-              <CardBody>
-                <h4 className={classes.cardTitle}>Симуляція у тривимірному просторі</h4>
-              </CardBody>
-              <CardFooter chart>
-                <div className={classes.stats}>1 симуляція</div>
-              </CardFooter>
-            </Card>
+            <Link to="3dviz">
+              <Card chart>
+                <CardHeader color="warning">
+                  <div className={classes.cardTitleWhite}>
+                    <h3>3D</h3>
+                  </div>
+                </CardHeader>
+                <CardBody>
+                  <h4 className={classes.cardTitle}>Симуляція у тривимірному просторі</h4>
+                </CardBody>
+                <CardFooter chart>
+                  <div className={classes.stats}>1 симуляція</div>
+                </CardFooter>
+              </Card>
+            </Link>
           </GridItem>
         </GridContainer>
       </div>
